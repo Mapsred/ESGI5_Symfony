1c3bee5e8d6b76f9dc20e1621f563bdf11d3b2dc
AxelE <axel.evrard@outlook.fr>
2018-12-05 10:39:08 +0100

    Feature#8 | Second BattlePet Fixture (dependencies)

--- src/DataFixtures/BattlePetTwoFixtures.php
+++ src/DataFixtures/BattlePetTwoFixtures.php
@@ -6,16 +6,16 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\BattlePet;
 
-class AppFixtures extends Fixture
+class BattlePetTwoFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $battlePet = new BattlePet();
 
-        $battlePet->setName("Idole d’Anubisath");
-        $battlePet->setIcon("inv_qirajidol_strife");
+        $battlePet->setName("Idole d’Anubis");
+        $battlePet->setIcon("inv_idol_strife");
         $battlePet->setSpellId(135267);
-        $battlePet->setCreatureId(68659);
+        $battlePet->setCreatureId(68660);
         $battlePet->setItemId(93040);
         $battlePet->setQualityId(3);
         $battlePet->setBreedId(6);
@@ -23,13 +23,20 @@ class AppFixtures extends Fixture
         $battlePet->setPetQualityId(3);
         $battlePet->setLevel(25);
         $battlePet->setHealth(1725);
-        $battlePet->setPower(276);
-        $battlePet->setSpeed(244);
-        $battlePet->setBattlePetGuid("000000000BB1CCF9");
-        $battlePet->setCreatureName("Idole d’Anubisath");
+        $battlePet->setPower(280);
+        $battlePet->setSpeed(245);
+        $battlePet->setBattlePetGuid("000000000BB1CCF8");
+        $battlePet->setCreatureName("Idole d’Anubis");
         $battlePet->setCanBattle(true);
         $manager->persist($battlePet);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            BattlePet::class,
+        );
     }
 }
