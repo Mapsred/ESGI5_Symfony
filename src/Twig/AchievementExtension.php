0d900274f259c507554864b1f813dec92470ac12
Laurent Bassin <laurent@bassin.info>
2019-01-11 11:21:22 +0100

    Display achievement title

---
+++ src/Twig/AchievementExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Twig;
+
+use App\Utils\BattleNetSDK;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AchievementExtension extends AbstractExtension
+{
+    /**
+     *
+     */
+    private $battleNetSDK;
+
+    /**
+     * AchievementExtension constructor.
+     * @param BattleNetSDK $battleNetSDK
+     */
+    public function __construct(BattleNetSDK $battleNetSDK)
+    {
+        $this->battleNetSDK = $battleNetSDK;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('filter_name', [$this, 'achievement_label'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('achievement_label', [$this, 'achievement_label']),
+        ];
+    }
+
+    public function achievement_label($id)
+    {
+        $achievement = $this->battleNetSDK->getAchievement($id);
+
+        return $achievement['title'] ?? '';
+    }
+}
