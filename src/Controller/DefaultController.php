ebcfdaf62d7c39062a70b3cd4c295dc1f22fca89
François Mathieu <maps_red@yahoo.fr>
2019-01-24 19:13:56 +0100

    Fix homepage and update composer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\BattlePet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\BattlePet;
 
 /**
  * Class DefaultController
@@ -23,13 +23,10 @@ class DefaultController extends AbstractController
         $battlePet = $this->getDoctrine()->getRepository(BattlePet::class)->find($id);
 
         if (!$battlePet) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            throw $this->createNotFoundException('No product found for id ' . $id);
         }
 
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
             'BattlePet' => $battlePet
         ]);
     }
