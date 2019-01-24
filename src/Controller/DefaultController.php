9046f38f0d8fd3938ec9aa53578b07eaea8b4fde
François Mathieu <maps_red@yahoo.fr>
2019-01-24 18:21:18 +0100

    Update DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Utils\BattleNetSDK;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,25 +14,12 @@ class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * @return Response
      */
-    public function index(Request $request)
+    public function index()
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
-
-    /**
-     * @Route("/authorize", name="authorize")
-     * @param BattleNetSDK $battleNetSDK
-     * @return Response
-     */
-    public function authorize(BattleNetSDK $battleNetSDK)
-    {
-//        var_dump($battleNetSDK->getRealms());
-        var_dump($battleNetSDK->getRealm('hyjal'));
-
-        return new Response();
-    }
-
 }
